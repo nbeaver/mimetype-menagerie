ee2ba53fe12129e59a04673af469bc22692bfcdd
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-10-27 13:32:54 -0500

    Check for broken urls.

---
+++ check-uris.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+import json
+import urllib.request
+
+mimetype_dict = json.load(open('uri-list.json'))
+
+for _, subtype_dict in mimetype_dict.items():
+    for _, url_list in subtype_dict.items():
+        for url in url_list:
+            # TODO: check urls in parallel?
+            try:
+                f = urllib.request.urlopen(url)
+            except urllib.error.HTTPError as err:
+                # TODO: url may redirect to err.url; display this too?
+                print("URL failed with status {}: {}".format(err.code, url))
