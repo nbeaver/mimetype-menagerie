46a15c167a13e501aa454c7b126c541034780d25
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-10-28 10:58:37 -0500

    Make URL checking more robust.

--- check-urls.py
+++ check-urls.py
@@ -4,14 +4,23 @@ import json
 import urllib.request
 import sys
 
-mimetype_dict = json.load(open(sys.argv[1]))
+if len(sys.argv) > 1:
+    mimetype_dict = json.load(open(sys.argv[1]))
+else:
+    print("Usage: {} urls.json".format(sys.argv[0]))
+    sys.exit(1)
 
 for _, subtype_dict in mimetype_dict.items():
     for _, url_list in subtype_dict.items():
         for url in url_list:
-            # TODO: check urls in parallel?
+            # TODO: check urls in parallel
+            f = None
             try:
                 f = urllib.request.urlopen(url)
             except urllib.error.HTTPError as err:
                 # TODO: url may redirect to err.url; display this too?
                 print("URL failed with status {}: {}".format(err.code, url))
+                continue
+            except ConnectionResetError as err:
+                print("Connection failed on: {}".format(err.code, url))
+                continue
