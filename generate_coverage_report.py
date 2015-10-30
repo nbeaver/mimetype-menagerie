52094ba686fdf36ed57a231b45aebdc93c8db614
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-10-29 21:15:21 -0500

    Primitive working version of coverage report.

--- generate_coverage_report.py
+++ generate_coverage_report.py
@@ -3,18 +3,37 @@
 import csv
 import glob
 import collections
+import os
 
-registered_mimetypes = collections.OrderedDict()
+mimetypes_iana = collections.OrderedDict()
 
-for filename in glob.glob("iana/*.csv"):
-    with open(filename) as csvfile:
-        media_type = filename.rstrip('.csv')
-        # e.g. registered_mimetypes['application'] = []
-        registered_mimetypes[media_type] = []
+for path in glob.glob("iana/*.csv"):
+    with open(path) as csvfile:
+        # This is a bit of a hack,
+        # but it works since the filenames match the media type.
+        media_type = os.path.basename(path).rstrip('.csv')
+        # e.g. mimetypes_iana['application'] = []
+        mimetypes_iana[media_type] = []
         reader = csv.DictReader(csvfile)
         for row in reader:
             subtype = row['Name']
-            registered_mimetypes[media_type].append(subtype)
+            mimetypes_iana[media_type].append(subtype)
 
-for media_type, subtypes in registered_mimetypes.items():
+print('Mimetypes registered:')
+for media_type, subtypes in mimetypes_iana.items():
+    print(media_type, len(subtypes))
+
+mimetypes_in_repo = {}
+
+for media_type in os.listdir('media-types/'):
+    mimetypes_in_repo[media_type] = []
+
+for dirpath, dirnames, filenames in os.walk('media-types/'):
+    for filename in filenames:
+        temp, subtype = os.path.split(dirpath)
+        _, media_type = os.path.split(temp)
+        mimetypes_in_repo[media_type].append(subtype)
+
+print('Mimetypes in repo:')
+for media_type, subtypes in mimetypes_in_repo.items():
     print(media_type, len(subtypes))
