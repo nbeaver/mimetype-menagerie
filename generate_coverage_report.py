44f1b94d583e0dd78185809de42d82f6a8ce4b3e
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-10-29 22:04:59 -0500

    More thorough coverage report.

--- generate_coverage_report.py
+++ generate_coverage_report.py
@@ -25,20 +25,39 @@ for dirpath, dirnames, filenames in os.walk('media-types/'):
     for filename in filenames:
         temp, subtype = os.path.split(dirpath)
         _, media_type = os.path.split(temp)
+        # TODO: make this more efficient
+        # by checking if there is at least one file in the directory
+        # instead of going through every single file.
         if media_type not in mimetypes_in_repo:
             mimetypes_in_repo[media_type] = set()
         else:
             mimetypes_in_repo[media_type].add(subtype)
 
-for media_type, subtypes in mimetypes_iana.items():
+total_iana = 0
+total_in_common = 0
+total_to_do = 0
+total_unregistered = 0
+for media_type, subtypes_iana in mimetypes_iana.items():
+    total_iana += len(subtypes_iana)
     if media_type in mimetypes_in_repo:
         subtypes_in_repo = mimetypes_in_repo[media_type]
     else:
         subtypes_in_repo = set()
-    mimetypes_common = set.intersection(subtypes, subtypes_in_repo)
-    mimetypes_unregistered = subtypes_in_repo - subtypes
-    mimetypes_to_do = subtypes - subtypes_in_repo
+    subtypes_in_common = set.intersection(subtypes_iana, subtypes_in_repo)
+    total_in_common += len(subtypes_in_common)
+    percent_in_common = 100*len(subtypes_in_common)/len(subtypes_iana)
+    subtypes_unregistered = subtypes_in_repo - subtypes_iana
+    total_unregistered += len(subtypes_unregistered)
+    subtypes_to_do = subtypes_iana - subtypes_in_repo
+    total_to_do += len(subtypes_to_do)
+    percent_to_do = 100*len(subtypes_to_do)/len(subtypes_iana)
     print(media_type)
-    print("    Mimetypes with examples:", len(mimetypes_common))
-    print("    Mimetypes that need examples:", len(mimetypes_to_do))
-    print("    Mimetypes unregistered:", len(mimetypes_unregistered))
+    print("    Mimetypes with examples: {} ({:.2f}%)".format(len(subtypes_in_common), percent_in_common) )
+    print("    Mimetypes that need examples: {} ({:.2f}%)".format(len(subtypes_to_do), percent_to_do) )
+    print("    Mimetypes examples not registered in IANA:", len(subtypes_unregistered) )
+
+total_percent_in_common = 100*total_in_common/total_iana
+total_percent_to_do = 100*total_to_do/total_iana
+print("Total IANA mimetypes with examples: {} ({:.2f}%)".format(total_in_common, total_percent_in_common) )
+print("Total IANA mimetypes in need of examples: {} ({:.2f}%)".format(total_to_do, total_percent_to_do) )
+print("Total mimetypes not registered in IANA:", total_unregistered )
