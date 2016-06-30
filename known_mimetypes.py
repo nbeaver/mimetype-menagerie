aa0fac79336a7bfb5364b2be572f255bab491423
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-06-30 15:36:26 -0500

    More consistent method.

--- known_mimetypes.py
+++ known_mimetypes.py
@@ -3,6 +3,8 @@
 from __future__ import print_function
 import os
 import sys
+import mimetypes
+
 
 if len(sys.argv) > 1:
     rootdir = sys.argv[1]
@@ -10,10 +12,12 @@ else:
     sys.stderr.write("Usage: {} media-types/\n".format(sys.argv[0]))
     sys.exit(1)
 
+known_mimetypes = set()
 for dirpath, dirnames, filenames in os.walk(rootdir):
-    if filenames:
-        # If there are files, we know we are in a directory with examples,
-        # so we add the media type to the list.
-        temp, subtype = os.path.split(dirpath)
-        _, media_type = os.path.split(temp)
-        print(media_type + '/' + subtype)
+    for filename in filenames:
+        mimetype, encoding = mimetypes.guess_type(filename)
+        if mimetype != None:
+            known_mimetypes.add(mimetype)
+
+for mimetype in sorted(known_mimetypes):
+    print(mimetype)
