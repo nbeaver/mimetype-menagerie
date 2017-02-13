95044f4b0163ebefbfa0f7b2f5a7bf1ca17fd958
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2017-02-13 12:18:19 -0600

    Make it run on the fly.

--- find_new_mimetypes.py
+++ find_new_mimetypes.py
@@ -16,20 +16,15 @@ if __name__ == '__main__':
         sys.exit(1)
 
     known_mimetypes = set()
-    with open(args.known_mimetypes_filepath) as mimefile:
+    with args.known_mimetypes_filepath as mimefile:
         for line in mimefile.readlines():
             known_mimetypes.add(line.strip())
 
-    new_mimetypes = {}
+    new_mimetypes = set()
     for dirpath, dirnames, filenames in os.walk(args.rootdir):
         for filename in filenames:
             filepath = os.path.join(dirpath, filename)
             mimetype, encoding = mimetypes.guess_type(filename)
-            if mimetype and mimetype not in known_mimetypes:
-                if mimetype not in new_mimetypes:
-                    new_mimetypes[mimetype] = []
-                new_mimetypes[mimetype].append(filepath)
-
-    for mimetype in sorted(new_mimetypes.keys()):
-        for path in new_mimetypes[mimetype]:
-            print(mimetype, path)
+            if mimetype is not None and mimetype not in known_mimetypes and mimetype not in new_mimetypes:
+                new_mimetypes.add(mimetype)
+                sys.stdout.write('{}\t{}\n'.format(mimetype, filepath))
