925ee6bd26835367af6003d9fe54aef9ed9a28a8
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2017-02-13 12:03:28 -0600

    Nicer argument handling.

--- find_new_mimetypes.py
+++ find_new_mimetypes.py
@@ -4,22 +4,24 @@ from __future__ import print_function
 import os
 import sys
 import mimetypes
+import argparse
 
 if __name__ == '__main__':
-    try:
-        mimepath = sys.argv[1]
-        rootdir = sys.argv[2]
-    except IndexError:
-        sys.stderr.write("Usage: {} known-mimetypes.txt /path/of/interest\n".format(sys.argv[0]))
+    parser = argparse.ArgumentParser(description='For discovering mimetypes not already in the mimetype menagerie')
+    parser.add_argument('known_mimetypes_filepath', type=argparse.FileType('r'), help='Path to text file with list of known mimetypes.')
+    parser.add_argument('rootdir', help='Root directory to start looking for new mimetypes.')
+    args = parser.parse_args()
+    if not os.path.isdir(args.rootdir):
+        sys.stderr.write("Error: not a directory: {}\n".format(args.rootdir))
         sys.exit(1)
 
     known_mimetypes = set()
-    with open(mimepath) as mimefile:
+    with open(args.known_mimetypes_filepath) as mimefile:
         for line in mimefile.readlines():
             known_mimetypes.add(line.strip())
 
     new_mimetypes = {}
-    for dirpath, dirnames, filenames in os.walk(rootdir):
+    for dirpath, dirnames, filenames in os.walk(args.rootdir):
         for filename in filenames:
             filepath = os.path.join(dirpath, filename)
             mimetype, encoding = mimetypes.guess_type(filename)
